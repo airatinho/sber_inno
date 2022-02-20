590856d6354504ed11885136448b2cf469aec9b1
karim <123>
2022-02-20 19:25:20 +0300

    redaction

--- task1.py
+++ task1.py
@@ -1,21 +1,28 @@
-def calc_v(height:list)->int:
+def calc_v(height: list, vol=0) -> int:
     """Возвращает заполняемый объем"""
 
-    #убираем максимум одиночку и присваиваем ему значение второго максимума
-    if height.count(max(height))==1:
-        height[height.index(max(height))]=sorted(height)[-2]
+    # убираем максимум одиночку и присваиваем ему значение второго максимума
 
-    #производим рассчет объема
+    if len(height) > 1:
+        if height.count(max(height)) == 1:
+            height[height.index(max(height))] = sorted(height)[-2]
+    else:
+        return vol
+    # производим рассчет объема
     max_height = 0
-    ind_max_heigh=0
-    vol=0
-    for ind,val in enumerate(height):
+    ind_max_heigh = 0
 
-        if val>=max_height:
-            if max_height!=0:
-                if (ind-ind_max_heigh)>1:
-                    for i in range(ind_max_heigh,ind):
-                        vol+=max_height-height[i]
-            ind_max_heigh=ind
-            max_height=val
+    for ind, val in enumerate(height):
+
+        if val >= max_height:
+
+            if (ind - ind_max_heigh) > 1:
+                for i in range(ind_max_heigh, ind):
+                    vol += max_height - height[i]
+                return calc_v(height[ind:], vol=vol)
+            else:
+                ind_max_heigh = ind
+                max_height = val
     return vol
+my_list=[0,1,0,2,1,0,1,3,2,1,2,1]
+print(calc_v(my_list))
